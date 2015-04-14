fa16a0b5f55200ffc87006383d81a4875ec2802a
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-04-14 15:03:52 -0700

    Update atomComboFinalLength.R

--- atomComboFinalLength.R
+++ atomComboFinalLength.R
@@ -1,6 +1,6 @@
 #Mode refers to the following:
 #Enumerate the combinations of atoms based on supplying the final atom string length.
-atomComboFinalLength<-function(atomlist,finallength){
+atomComboFinalLength<-function(atomlist,finallength,){
   atomlist<-as.character(atomlist)
   finallength<-as.numeric(finallength)
   listing<-list(list())
@@ -8,4 +8,6 @@ atomComboFinalLength<-function(atomlist,finallength){
     listing[[i]]<-atomlist
   }  
   atomstrings<-do.call(paste0,expand.grid(listing))
+  mirrorimages<-unlist(lapply(strsplit(atomstrings,""),function(x) paste(rev(x),collapse="")))
+  uniques<-unique(c(atomstrings,mirrorimages))
 }
