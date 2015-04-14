f5c0d87bc4659cbfd5eac9dd2eaf9ce9b2e0df6d
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-04-14 14:04:55 -0700

    Create atomcombo.R

---
+++ atomcombo.R
@@ -0,0 +1,9 @@
+#Mode refers to the following:
+#Enumerate the combinations of atoms
+atomcombo<-function(atomlist,length,mode=,){
+  atomlist<-as.character(atomlist)
+  finallength<-as.numeric(finallength)
+  
+  do.call(paste0,expand.grid())
+  
+}
